e23e9f43869d1bea86abd5244e96250780e97874
philip <pmutua@live.com>
2021-06-02 13:21:18 +0300

    feat: implemented view for uploading a menu

--- app/api/views.py
+++ app/api/views.py
@@ -139,6 +139,7 @@ class UserLoginAPIView(APIView):
 
 class CreateRestaurantAPIView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
+
     def post(self, request, format=None):
         req = request.data
         user = {'created_by': jwt_decode_handler(request.auth).get('username')}
@@ -152,3 +153,36 @@ class CreateRestaurantAPIView(APIView):
 
         res = {"msg": str(serializer.errors), "success": False, "data": None}
         return Response(data=res, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UploadMenuAPIView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request):
+
+        try:
+            req = request.data.dict()
+            todays_date = settings.CURRENT_DATE.date()
+            menu = Menu.objects.filter(Q(restaurant__id=int(req.get('restaurant')))
+                                       and Q(created_at__date__iexact=todays_date))
+            user = jwt_decode_handler(request.auth).get('username')
+
+            if menu.exists():
+                res = {
+                    "msg": "Menu already added.You can add another one tommorrow or update the exsting one that was created today.",
+                    "success": False,
+                    "data": None}
+                return Response(data=res, status=status.HTTP_200_OK)
+
+            serializer = UploadMenuSerializer(data=req)
+
+            if serializer.is_valid():
+                serializer.save(uploaded_by=user)
+                res = {"msg": "Menu uploaded", "success": True, "data": serializer.data}
+                return Response(data=res, status=status.HTTP_201_CREATED)
+
+            res = {"msg": str(serializer.errors), "success": False, "data": None}
+            return Response(data=res, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            res = {"msg": str(e), "success": False, "data": None}
+            return Response(data=res, status=status.HTTP_400_BAD_REQUEST)
