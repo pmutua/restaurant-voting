0fcbc784563d5fed4400f6fb73df787690f3551d
philip <pmutua@live.com>
2021-06-02 14:29:02 +0300

    fixed: Pagination may yield inconsistent results with an unordered object_list

--- app/api/models.py
+++ app/api/models.py
@@ -53,18 +53,19 @@ class Restaurant(models.Model):
     """Represents restaurant class model"""
     name = models.CharField(unique=True, max_length=255, blank=True, null=True)
     contact_no = models.CharField(
-        unique=True,
         max_length=255,
         blank=True,
         null=True)
     address = models.CharField(
-        unique=True,
         max_length=255,
         blank=True,
         null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_by = models.CharField(max_length=50, null=True, blank=True)
+
+    class Meta:
+        ordering = ['-id']
 
     def __str__(self):
         return self.name
@@ -72,15 +73,11 @@ class Restaurant(models.Model):
 
 class Menu(models.Model):
     """Represents menu class model"""
-    restaurant = models.CharField(
-        unique=True,
-        max_length=255,
-        blank=True,
-        null=True)
+    restaurant = models.ForeignKey(Restaurant,null=True,blank=True,on_delete=models.CASCADE)
     file = models.FileField(upload_to='menus/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.CharField(max_length=50, null=True, blank=True)
+    uploaded_by = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return f'File id: {self.id}'
