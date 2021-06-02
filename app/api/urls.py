8bfbb070f03013bf2c92abe8c681305246ab10b9
philip <pmutua@live.com>
2021-06-02 16:45:31 +0300

    feat: Added route: url to vote

--- app/api/urls.py
+++ app/api/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('create_employee/', CreateEmployeeAPIView.as_view(), name="create-employee"),
     path('restaurants/', RestaurantListAPIView.as_view(), name="restaurants"),
     path('menu_list/', CurrentDayMenuList.as_view(), name="menu-list"),
+    path('vote/<int:menu_id>/', VoteAPIView.as_view(), name="new-vote"),
 
 
 
