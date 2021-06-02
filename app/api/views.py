0afede37d6d688450a75df78ff5a1d8c5aa73ede
philip <pmutua@live.com>
2021-06-02 14:11:25 +0300

    feat: Added view for creating new employee

--- app/api/views.py
+++ app/api/views.py
@@ -186,3 +186,89 @@ class UploadMenuAPIView(APIView):
         except Exception as e:
             res = {"msg": str(e), "success": False, "data": None}
             return Response(data=res, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CreateEmployeeAPIView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request):
+        req = request.data
+
+        user = jwt_decode_handler(request.auth).get('username')
+        employee_no = req.get('employee_no')
+        employee = Employee.objects.filter(
+            Q(employee_no=employee_no)
+        )
+        if employee.exists():
+            res = {"msg": f"Employee with EMPLOYEE NO { employee_no } already exists", "data": None, "success": False}
+            return Response(data=res, status=status.HTTP_400_BAD_REQUEST)
+
+        role_group, _ = Role.objects.get_or_create(name=req.get('employee'))
+
+        serializer = EmployeeSerializer(data=req)
+
+        if serializer.is_valid():
+            try:
+                new_user = User.objects.create(
+                    username=req.get('email'),
+                    email=req.get('email'),
+                    first_name=req.get('first_name').capitalize(),
+                    last_name=req.get('last_name').capitalize(),
+                    is_active=True,
+                    phone=req.get('phone'),
+                    identification_no=req.get('identification_no'),
+                    is_staff=True,
+                    created_by=user
+
+                )
+
+                new_user.roles.add(role_group)
+
+                password = User.objects.make_random_password(length=10)
+                new_user.set_password(password)
+                new_user.save()
+
+                Employee.objects.create(
+                    user=new_user,
+                    employee_no=req.get('employee_no'),
+                    created_by=user
+                )
+
+                login_site = settings.LOGIN_REDIRECT_URL
+
+                ctx = {
+                    "username": new_user.username,
+                    "password": password,
+                    "link": login_site,
+                    'user_email': new_user.email
+                }
+
+                client_html_message = loader.render_to_string('api/email-user-creds.html')
+
+                client_message_string = strip_tags(client_html_message)
+
+                client_html_content = loader.render_to_string('api/email-user-creds.html', ctx)
+
+                client_message = {
+                    "html": client_html_content,
+                    "text": client_message_string
+                }
+
+                serializer = UserSerializer(new_user)
+
+                send_mail_to_admin = send_credentials(
+                    'Welcome onboard',
+                    client_message,
+                    new_user.email
+                )
+
+                res = {
+                    "msg": "Employee successfully created. Login information has been sent to employee's email",
+                    "data": serializer.data,
+                    "success": True}
+                return Response(data=res, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                res = {"msg": str(e), "data": None, "success": False}
+                return Response(data=res, status=status.HTTP_400_BAD_REQUEST)
+        res = {"msg": str(serializer.errors), "data": None, "success": False}
+        return Response(data=res, status=status.HTTP_400_BAD_REQUEST)
