02694105785f9505dc35c311085bce487b27946e
philip <pmutua@live.com>
2021-06-02 11:17:36 +0300

    feat: Add url for creating a restaurant

--- app/api/urls.py
+++ app/api/urls.py
@@ -6,7 +6,9 @@ app_name = 'api'
 
 urlpatterns = [
     path('register_user/',   RegisterUserAPIView.as_view(), name="register-user"),
-    path('user_login/', UserLoginAPIView.as_view(), name="user-login"),  
+    path('user_login/', UserLoginAPIView.as_view(), name="user-login"),
+    path('create_restaurant/', CreateRestaurantAPIView.as_view(), name="create-restaurant"),  
+    
     
 ]
 
