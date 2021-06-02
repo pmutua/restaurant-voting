c5ff7840013eebd485eda3a10df06e78cc2c3651
philip <pmutua@live.com>
2021-06-02 14:14:17 +0300

    tests: Added test_create_employee

--- app/api/tests/api.py
+++ app/api/tests/api.py
@@ -97,6 +97,46 @@ class TestCreateUploadMenuAPI(APITestCase):
     def test_post_request_can_upload_menu(self):
 
         res = self.client.post(reverse("api:upload-menu"), data=self.payload, format="multipart")
-        print(res.json())
-        r = Menu.objects.all().last()
+
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_post_request_upload_menu_unauthenticated(self):
+
+        self.client.force_authenticate(user=None)
+
+        res = self.client.post(reverse("api:upload-menu"), data=self.payload, format="multipart")
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class TestCreateEmployeeAPI(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123')
+        self.payload = jwt_payload_handler(self.user)
+        self.token = jwt_encode_handler(self.payload).decode('UTF-8')
+        self.api_authentication()
+
+        self.payload = {
+            "email": "punakepis@dropjar.com ",
+            "first_name": "George",
+            "last_name": "Lucas",
+            "phone": "+2404554444",
+            "identification_no": "5343434242",
+            "employee_no": "XV-098335"
+
+        }
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+
+    def test_post_request_can_create_new_employee(self):
+
+        res = self.client.post(reverse("api:create-employee"), data=self.payload, format="json")
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_post_request_create_new_employee_unauthenticated(self):
+
+        self.client.force_authenticate(user=None)
+
+        res = self.client.post(reverse("api:create-employee"), data=self.payload, format="json")
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
