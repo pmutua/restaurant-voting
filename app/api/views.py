aff7d964a44c763b8e15952f9e0e5598337a2840
philip <pmutua@live.com>
2021-06-04 10:33:19 +0300

    feat: Added logout view.

--- app/api/views.py
+++ app/api/views.py
@@ -141,6 +141,21 @@ class UserLoginAPIView(APIView):
             return Response(data=res, status=status.HTTP_200_OK)
 
 
+class UserLogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        try:
+            username = jwt_decode_handler(request.auth).get('username')
+            user = User.objects.get(username=username)
+            payload = jwt_payload_handler(user)
+            jwt_encode_handler(payload)
+            res = {"msg": "User logged out successfully", "success": True, "data": None}
+            return Response(data=res,status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
 class CreateRestaurantAPIView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
 
