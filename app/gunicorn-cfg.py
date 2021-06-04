a1f7e90e81f6aa029ff6b27d060fdc59f85b4995
philip <pmutua@live.com>
2021-06-04 11:24:56 +0300

    Added gunicorn-cfg.py file to be used when running docker entrypoint

---
+++ app/gunicorn-cfg.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2021 - present Philip Mutua
+"""
+
+bind = '0.0.0.0:5006'
+workers = 4
+timeout = 600
+accesslog = '-'
+loglevel = 'debug'
+capture_output = True
+enable_stdio_inheritance = True
+
+
+
