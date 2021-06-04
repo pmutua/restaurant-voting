2b6e251f1dcf744afdfd8fa12913829037170efc
philip <pmutua@live.com>
2021-06-04 10:07:44 +0300

    configured default file storage as cloudinary service and codinary settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -257,3 +257,14 @@ EMAIL_USE_TLS = True
 
 
 LOGIN_REDIRECT_URL=config('LOGIN_REDIRECT_URL')
+
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+
+
