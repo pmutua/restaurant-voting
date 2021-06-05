ea0d2d46c35401dc210f81cfd22cd1d7b6b0012e
philip <pmutua@live.com>
2021-06-05 09:14:59 +0300

    Add sengrid sender email settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -242,7 +242,7 @@ LOGGING = {
 
 
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
