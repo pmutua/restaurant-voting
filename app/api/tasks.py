b4a99af37525fb050134b2da5aac38bc5fb1d7e3
philip <pmutua@live.com>
2021-06-05 09:22:19 +0300

    use sender email from settings

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -1,10 +1,13 @@
 
 from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+
+SENDER_EMAIL = settings.SENDGRID_SENDER_EMAIL
 
 
 def send_credentials(subject, message, org_email):
     """Sends Email to User with Credentials."""
-    subject, from_email, to = subject, 'philip@nouveta.tech', org_email
+    subject, from_email, to = subject, SENDER_EMAIL, org_email
     text_content = message.get('text')
     html_content = message.get('html')
 
